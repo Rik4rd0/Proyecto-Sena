02fd15f114e3efeb07f064c8d89a34b00ad109c7
Rik4rd0 <80051019+Rik4rd0@users.noreply.github.com>
2022-08-10 12:34:57 -0500

    Inicio
    Tienda

---
+++ js/slider.js
@@ -0,0 +1,36 @@
+const slider=document.querySelector('.slider');
+let sliderSection= document.querySelectorAll('.slider__section');
+let sliderSectionLast= sliderSection[sliderSection.length-1];
+
+const btnLeft=document.querySelector('#btn-left');
+const btnRight=document.querySelector('#btn-right');
+
+slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+
+function next(){
+    let sliderSectionFirst= document.querySelector('.slider__section')[0];
+    slider.style.marginLeft='-200%';
+    slider.style.transition='all 0.5s';
+    setTimeout(function(){
+        slider.insertAdjacentElement('afterbegin',sliderSectionFirst);
+        slider.style.marginLeft='0';
+        slider.style.transition='all 0.5s';
+    },500);
+
+}
+function prev(){
+    let sliderSection = document.querySelectorAll('.slider__section');
+    let sliderSectionLast= sliderSection[sliderSection.length-1];
+    slider.style.marginLeft='0';
+    slider.style.transition='all 0.5s';
+    setTimeout(function(){
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+        slider.style.marginLeft='-100%';
+        slider.style.transition='none';
+    },500);
+}
+btnRight.addEventListener('click',next);
+
+btnLeft.addEventListener('click',prev);
+
+setInterval(next,5000);
